67f04f491ab600424ce5e88fc841e6d5c3dc1bdb
Melissa Gymrek <melissagymrek@Melissas-MacBook-Pro.local>
2022-08-02 12:58:16 -0700

    updating reference STR stats

--- analysis/stats.sh
+++ analysis/stats.sh
@@ -2,7 +2,19 @@
 
 # Compute other stats on BXD files
 
-REFSTRS=/gymreklab-tscc/mikhail/090520_unified_workflow/data/ref/str_regions.for_gangstr.bed
-echo "Number of reference loci..." $(cat $REFSTRS |  wc -l) # 1178073
-echo "Number of reference STRs..." $(cat $REFSTRS | awk '($4<=6)' |  wc -l) # 1156732
-echo "Number of reference VNTRs..." $(cat $REFSTRS | awk '($4>6)' |  wc -l) # 21341
+############## Reference TR set #################
+REFSTRS=/gymreklab-tscc/mikhail/090520_unified_workflow/data/ref/str_regions.bed
+MOTIFINFO=/gymreklab-tscc/mikhail/090520_unified_workflow/data/ref/motif_comp/str_regions_mm10_filt_w_hom.tsv.gz
+# Unique TRs: 1,176,016
+zcat $MOTIFINFO | grep -v motif | cut -f 1-4 | \
+  intersectBed -a $REFSTRS -b stdin -wa -wb -f 1 | \
+  awk '($2==$5 && $3==$6)' | wc -l
+# How many are STRs: 1,154,738
+zcat $MOTIFINFO | grep -v motif | cut -f 1-4 | \
+  intersectBed -a $REFSTRS -b stdin -wa -wb -f 1 | \
+  awk '($2==$5 && $3==$6)' | awk '($7<=6)' | wc -l
+# How many are VNTRs: 21,278
+zcat $MOTIFINFO | grep -v motif | cut -f 1-4 | \
+  intersectBed -a $REFSTRS -b stdin -wa -wb -f 1 | \
+  awk '($2==$5 && $3==$6)' | awk '($7>6)' | wc -l
+# Check:1154738+21278=1176016
